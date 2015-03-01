2dde12c609e39bf56d8d2fe48455337b9b4ff0a3
Vadim Yelagin <v.yelagin@eastbanctech.ru>
2015-03-01 21:28:16 +0600

    "Add Review" cell (does nothing)

--- Get Fueled/View/VenueDetails/VenueDetailsTableViewModel.m
+++ Get Fueled/View/VenueDetails/VenueDetailsTableViewModel.m
@@ -9,19 +9,40 @@
 #import "TextViewCellModel.h"
 #import "VenueDetailsOverviewCellModel.h"
 #import "VenueDetailsTableViewModel.h"
+#import <ETRCollectionModel/ETRStaticCellModel.h>
+
+@interface VenueDetailsTableViewModel ()
+
+@property (nonatomic, strong) id addReviewItem;
+
+@end
 
 @implementation VenueDetailsTableViewModel
 
 - (instancetype)initWithVenue:(Venue *)venue
 {
-    TextViewCellModel *newReviewTextView = [[TextViewCellModel alloc] init];
     VenueDetailsOverviewCellModel *overview = [[VenueDetailsOverviewCellModel alloc] initWithVenue:venue];
-    NSArray *items = @[overview, newReviewTextView];
+
+    TextViewCellModel *newReviewTextView = [[TextViewCellModel alloc] init];
+    ETRStaticCellModel *addReview = [[ETRStaticCellModel alloc] initWithReuseIdentifier:@"AddReviewCell"];
+    
+    NSArray *items = @[overview, newReviewTextView, addReview];
     self = [super initWithSections:@[items]];
     if (self) {
         _venue = venue;
+        _addReviewItem = addReview;
     }
     return self;
+}
+
+- (void)didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+                      completion:(void (^)(void))completion
+{
+    id item = [self itemAtIndexPath:indexPath];
+    if (item == self.addReviewItem) {
+        if (completion)
+            completion();
+    }
 }
 
 @end
