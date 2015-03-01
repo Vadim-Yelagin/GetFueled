4fef2df98a9c1a83242b46ffe71335fe88bfe67b
Vadim Yelagin <v.yelagin@eastbanctech.ru>
2015-03-01 21:20:52 +0600

    Dynamic TextViewCell height

--- Get Fueled/View/VenueDetails/TextViewCell.m
+++ Get Fueled/View/VenueDetails/TextViewCell.m
@@ -8,11 +8,13 @@
 
 #import "TextViewCell.h"
 #import "TextViewCellModel.h"
+#import <ETRCollectionModel/ETRTableViewController.h>
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
-@interface TextViewCell ()
+@interface TextViewCell () <UITextViewDelegate>
 
 @property (nonatomic, strong) IBOutlet UITextView *textView;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint *textViewHeight;
 
 @end
 
@@ -21,8 +23,35 @@
 - (void)awakeFromNib
 {
     [super awakeFromNib];
-    RAC(self.textView, text) = RACObserve(self, viewModel.text);
-    RAC(self, viewModel.text) = [self.textView.rac_textSignal skip:1];
+    RACSignal *modelText = RACObserve(self, viewModel.text);
+    [self rac_liftSelector:@selector(setText:)
+               withSignals:modelText, nil];
+}
+
+- (void)setText:(NSString *)text
+{
+    if ([text isEqual:self.textView.text])
+        return;
+    self.textView.text = text;
+    [self updateHeight];
+}
+
+- (void)updateHeight
+{
+    [self.textView layoutSubviews];
+    CGFloat newHeight = self.textView.contentSize.height + 30;
+    if (self.textViewHeight.constant == newHeight)
+        return;
+    self.textViewHeight.constant = newHeight;
+    UITableView *tableView = self.tableViewController.tableView;
+    [tableView beginUpdates];
+    [tableView endUpdates];
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    self.viewModel.text = textView.text;
+    [self updateHeight];
 }
 
 @end
