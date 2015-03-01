611162f3691610a05e41b8eaf49883555756841e
Vadim Yelagin <v.yelagin@eastbanctech.ru>
2015-03-01 14:46:48 +0600

    Initial Commit

---
+++ Get Fueled/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Get Fueled
+//
+//  Created by Vadim Yelagin on 01/03/15.
+//  Copyright (c) 2015 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
