0a531e0a79364f393d99a157ab66a2643d667f0d
Vadim Yelagin <v.yelagin@eastbanctech.ru>
2015-03-01 15:29:01 +0600

    Initial core data model, bare Venue model class.

---
+++ Get Fueled/Model/Venue.h
@@ -0,0 +1,18 @@
+//
+//  Venue.h
+//  Get Fueled
+//
+//  Created by Vadim Yelagin on 01/03/15.
+//  Copyright (c) 2015 Fueled. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Venue : NSManagedObject
+
+@property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ Get Fueled/Model/Venue.m
@@ -0,0 +1,17 @@
+//
+//  Venue.m
+//  Get Fueled
+//
+//  Created by Vadim Yelagin on 01/03/15.
+//  Copyright (c) 2015 Fueled. All rights reserved.
+//
+
+#import "Venue.h"
+
+
+@implementation Venue
+
+@dynamic identifier;
+@dynamic name;
+
+@end
